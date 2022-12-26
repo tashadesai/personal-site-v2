913e95767a0d5cf667a7a94b62c760da417578dc
Natasha Desai <natashadesai@natashas-mbp.lan>
2022-12-27 02:18:58 +0800

    animation update

--- src/App.js
+++ src/App.js
@@ -29,18 +29,18 @@ class App extends React.Component {
         var card = document.querySelector("#sticker");
         var playing = false;
 
-        card.addEventListener('click',function() {
-            if(playing)
+        card.addEventListener('click', function () {
+            if (playing)
                 return;
 
             playing = true;
             anime({
                 targets: card,
-                scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
+                scale: [{value: 1}, {value: 1.1}, {value: 1, delay: 250}],
                 rotateY: {value: '+=180', delay: 200},
                 easing: 'easeInOutSine',
-                duration: 400,
-                complete: function(anim){
+                duration: 500,
+                complete: function (anim) {
                     playing = false;
                 }
             });
@@ -50,7 +50,7 @@ class App extends React.Component {
 
     render() {
         return (
-            <div className="App mw9 center ph6-ns pt5-ns">
+            <div className="App mw9 center ph6-ns pt6-ns">
                 <div id="hi" className="intro fl w-100 h4">
                     Hi, I'm
                 </div>
@@ -63,30 +63,37 @@ class App extends React.Component {
                         Currently, I'm a software engineering manager at Innovid.
                     </div>
                 </div>
-                <div className="links fr w-80 pt6">
+
+                <div className="fl w-20" style={{"padding-left": "-1rem"}}>
+                    <div id="sticker">
+                        <div className="card">
+                            <div className="front">
+                            </div>
+                            <div className="back">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="links fr w-80">
                     <div className="fl w-100 w-25-ns pa2">
-                        <div>Resume</div>
                         <div id="resu" className={"bubble"}>
                             Resume
                             <span></span><span></span><span></span><span></span>
                         </div>
                     </div>
                     <div className="fl w-100 w-25-ns pa2">
-                        <div>LinkedIn</div>
                         <div id="linke" className={"bubble"}>
                             LinkedIn
                             <span></span><span></span><span></span><span></span>
                         </div>
                     </div>
                     <div className="fl w-100 w-25-ns pa2">
-                        <div>Github</div>
                         <div id="gith" className={"bubble"}>
                             Github
                             <span></span><span></span><span></span><span></span>
                         </div>
                     </div>
                     <div className="fl w-100 w-25-ns pa2">
-                        <div>Email</div>
                         <div id="email" className={"bubble"}>
                             Email
                             <span></span><span></span><span></span><span></span>
@@ -95,14 +102,7 @@ class App extends React.Component {
 
                 </div>
 
-                <div id="sticker">
-                    <div className="card">
-                        <div className="front">
-                        </div>
-                        <div className="back">
-                        </div>
-                    </div>
-                </div>
+
             </div>
         );
     }
